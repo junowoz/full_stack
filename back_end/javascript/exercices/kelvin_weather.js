ac04ce540e005c60d82987ccd76e81cbbf1c64c6
junowoz <junogouvea@gmail.com>
2022-06-14 23:48:55 -0400

    alt

--- back_end/javascript/exercices/kelvin_weather.js
+++
@@ -1,8 +0,0 @@
-//Kelvin Weather Converter
-
-let kelvin = 293 //Kelvin
-let celsius = kelvin - 273 //Celsius
-let fahrenheit = celsius * (9 / 5) + 32 //Fahrenheit
-fahrenheit = Math.floor(fahrenheit); //Turning Fahrenheit non-decimal
-
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
