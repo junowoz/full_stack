f01da4ced82f97d87ff86f468a1dd45b03eef8e9
junowoz <junogouvea@gmail.com>
2022-06-30 18:07:10 -0400

    react 29 jung

--- front_end/JSX/advanced_jsx.js
+++ front_end/JSX/advanced_jsx.js
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Write code here:
+let myDiv = <div className="big">I AM A BIG DIV</div>
+ReactDOM.render(myDiv, document.getElementById('app'))
