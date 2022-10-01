511cdb80466bcfca77bf8f409082bba423bb5188
junowoz <junogouvea@gmail.com>
2022-09-30 21:11:35 -0400

    sql

--- back_end/SQL/1_manipulation.sql
+++ back_end/SQL/1_manipulation.sql
@@ -1,12 +1,16 @@
 --Introduction to SQL: aqui estamos retornando informação de uma base de dados.
 SELECT * FROM celebs;
 
+------------------------------------------------------------------------------------
+
 --Statements
 CREATE TABLE table_name (
     column_1 data_type,
     column_2 data_type,
     column_3 data_type
 );
+
+------------------------------------------------------------------------------------
 
 --Create Table: means create a new table, celebs: name of the table
 CREATE TABLE celebs(
@@ -15,23 +19,18 @@ CREATE TABLE celebs(
     age INTEGER
 );
 
+------------------------------------------------------------------------------------
+
 --Insert: The INSERT statement inserts a new row into a table.
 --INSERT INTO: Adds rows
 --(id, name, age): parameter of data
 --VALUES: Following data will be inserted.
 --(a,b,c): The data
 
-INSERT INTO celebs (id, name, age)
-VALUES (1, 'Justin Bieber', 22);
+INSERT INTO table_name (column1, column2, column3) 
+VALUES (value1, value2, value3);
 
-INSERT INTO celebs (id, name, age) 
-VALUES (2, 'Beyonce Knowles', 33); 
- 
-INSERT INTO celebs (id, name, age) 
-VALUES (3, 'Jeremy Lin', 26); 
- 
-INSERT INTO celebs (id, name, age) 
-VALUES (4, 'Taylor Swift', 26);
+------------------------------------------------------------------------------------
 
 --Select: Is used to fetch data from a database. Return data.
 --SELECT:Is a clause that indicates that the statement is a query.
@@ -43,6 +42,8 @@ VALUES (4, 'Taylor Swift', 26);
 
 SELECT * FROM celebs;
 
+------------------------------------------------------------------------------------
+
 --Alter: Adds a new column to a table.
 --ALTER TABLE is a clause that lets you make the specified changes.
     --name: of the table.
@@ -50,10 +51,12 @@ SELECT * FROM celebs;
     --name of the column
     --TEXT is the data type for the new column
 
-ALTER TABLE celebs
-ADD COLUMN twitter_handle TEXT;
+ALTER TABLE table_name
+ADD COLUMN column DATA_TYPE;
 
 SELECT * FROM celebs;
+
+------------------------------------------------------------------------------------
 
 --UPDATE: This statement edits a row in a table.
 --SET is a clause that indicates the column to edit.
@@ -61,3 +64,62 @@ SELECT * FROM celebs;
 UPDATE celebs
 SET twitter_handle = '@taylorswift13' WHERE id = 4;
 SELECT * FROM celebs;
+
+------------------------------------------------------------------------------------
+
+--DELETE: Deletes rows from a table.
+    --DELETE FROM name_of_table
+    --WHERE name_row IS NULL;
+    --IS NULL: returns true when the value is NULL
+DELETE FROM celebs
+WHERE twitter_handle IS NULL;
+
+SELECT * FROM celebs;
+
+------------------------------------------------------------------------------------
+
+--CONSTRAINS: Adds information to the
+    --PRIMARY KEY columns can be used to uniquely identify the row
+    --UNIQUE columns have a different value for every row.
+    -- NOT NULL columns must have a value. 
+    --DEFAULT columns take an additional argument
+
+CREATE TABLE awards(
+    id INT PRIMARY KEY,
+    recipient TEXT NOT NULL,
+    award_name TEXT DEFAULT 'Grammy',
+);
+
+--------------------------------------------------------------
+--PROJECT | CREATE A TABLE
+
+CREATE TABLE friends(
+    id INTEGER,
+    name TEXT,
+    birthday DATE
+);
+
+INSERT INTO friends (id, name, birthday)
+VALUES (1, 'Ororo Munroe', '1940-05-30');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (2, 'Anne', '2003-07-08');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (3, 'Juan', '1947-08-10');
+
+UPDATE friends
+SET name = 'Storm' 
+WHERE id = 1;
+
+ALTER TABLE friends
+ADD COLUMN email TEXT;
+
+UPDATE friends
+SET email = 'storm@codecademy.com'
+WHERE id = 1;
+
+DELETE FROM friends
+WHERE id = 1;
+
+SELECT * FROM friends;
