de163b3a94e14d7ee6d5a021ca0732cbfdfa82c6
junowoz <junogouvea@gmail.com>
2022-12-12 21:13:45 -0500

    TRAINING

--- back_end/Node/1_node.js
+++ back_end/Node/1_node.js
@@ -1,7 +1,4 @@
-/*
-*
-* */
-
+/////////////////////////////////////////////////////////////////////////////////////
 /*
 * RUNNING A PROGRAM WITH NODE
 * */
@@ -19,6 +16,7 @@ let noun3 = 'potatoes';
 
 console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
 
+/////////////////////////////////////////////////////////////////////////////////////
 /*
 * CORE MODULES
 * */
@@ -30,6 +28,7 @@ console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who lov
 // For example: Require in the 'events' core module:
 const events = require('events');
 
+/////////////////////////////////////////////////////////////////////////////////////
 /*
 * The Console Module
 The console object provides many of the same familiar methods such as:
@@ -43,3 +42,149 @@ const petsArray = ['dog', 'cat', 'bird', 'monkey'];
 console.log(petsArray)
 console.table(petsArray)
 console.assert(petsArray.length > 5);
+
+/////////////////////////////////////////////////////////////////////////////////////
+/**
+ * The Process Module
+ */
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2]; //this inputs the word I choose before typing 'node 1_node.js ' in the terminal
+
+console.log(`Your word is ${word}`)
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time 
+for (let i = 0; i < 1000; i++){
+  wordArray.push(`${word} count: ${i}`)
+}
+
+console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+//then run with node 1_node.js
+
+/////////////////////////////////////////////////////////////////////////////////////
+/**
+ * The OS Module
+ */
+
+const os = require('os');
+ 
+const local = {  
+  'Home Directory': os.homedir(),    
+  'Operating System': os.type(),
+  'Last Reboot': os.uptime()
+}
+
+console.log(local);
+
+/////////////////////////////////////////////////////////////////////////////////////
+/**
+ * The Util Module
+ */
+// Require in trails module from trails.js
+const trails = require('./trails.js');
+// Require in util module here
+const util = require('util');
+// Simulate database call to search trails module for specified trail
+const getTrailDistance = (trail, callback) => {
+  return setTimeout(() => {
+    if (trails.hasOwnProperty(trail)) {    
+      const foundTrail = trails[trail];    
+      callback(null, foundTrail)
+    } else {
+      callback(new Error('Trail not found!'))
+    }
+  }, 1000);
+}
+
+// Callback function to send an error in the case of an error, or to handle trail data if a trail was found successfully.
+function callback (error, trailData) {
+  if (error) {
+    console.error(error.message)
+    process.exit(1)
+  } else {
+    const mi = trailData.miles;   
+    const nickname = trailData.nickname;
+    console.log(`The ${nickname} is ${mi} miles long!`)
+  }
+}
+
+getTrailDistance('North Country', callback)
+
+// Promisfy below!
+
+const getTrailDistancePromise = util.promisify(getTrailDistance)
+
+getTrailDistancePromise('North Country').then((foundTrail) => {      
+    const nickname = foundTrail.nickname;
+    const mi = foundTrail.miles; 
+    console.log(`The ${nickname} is ${mi} miles long!`);
+  }).catch((error) => {
+    console.log('Trail not found!', error);
+  })
+
+/////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * Implementing Modules in Node
+   */
+
+  /* EXPORT - module.exports*/
+function celsiusToFahrenheit(celsius) {
+  return celsius * (9/5) + 32;
+}
+ 
+module.exports.celsiusToFahrenheit = celsiusToFahrenheit;
+
+//Second option to export
+module.exports.fahrenheitToCelsius = function(fahrenheit) {
+  return (fahrenheit - 32) * (5/9);
+};
+
+/** IMPORT - require()**/
+const converters = require('./converters.js');
+ 
+const freezingPointC = 0;
+const boilingPointC = 100;
+ 
+const freezingPointF = converters.celsiusToFahrenheit(freezingPointC);
+const boilingPointF = converters.celsiusToFahrenheit(boilingPointC);
+ 
+console.log(`The freezing point of water in Fahrenheit is ${freezingPointF}`);
+console.log(`The boiling point of water in Fahrenheit is ${boilingPointF}`);
+
+//Using Object Destructuring to be more Selective With require()
+const { celsiusToFahrenheit } = require('./converters.js');
+// This way, we are choosing the specific celsiusToFahrenheit function inside converters.js
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Code Challenge
+/**
+ * Complete the shape-area.js module. Your module must declare and export two functions with the following signatures:
+    circleArea(radiusLength).
+    squareArea(sideLength).
+ */
+const PI = Math.PI;
+
+// Define and export circleArea() and squareArea() below
+
+module.exports.circleArea = function (radiusLength) {
+  return PI * radiusLength * radiusLength;
+}
+
+module.exports.squareArea = function (sideLength) {
+    return sideLength * sideLength;
+}
+
+//ALTERNATIVE FORM
+const PI = Math.PI;
+
+function circleArea(r) {
+  return PI * r * r;
+}
+function squareArea(side) {
+  return side * side;
+}
+
+module.exports.circleArea = circleArea;
+module.exports.squareArea = squareArea;
