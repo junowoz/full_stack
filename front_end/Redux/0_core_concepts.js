0173330904a432562ba272509119974c43fc2367
Juan José <junogouvea@gmail.com>
2023-01-18 19:57:46 -0400

    redux

---
+++ front_end/Redux/0_core_concepts.js
@@ -0,0 +1,8 @@
+//STATE: State of a To-Do App
+const state = ["Print trail map", "Pack snacks", "Summit the mountain"];
+
+//ACTIONS
+const action = {
+    type: "todos/addTodo",
+    payload: "Take selfies",
+}; 
