bd7073d1a702160e4c8ae0a7244dd8d91e1dc993
Juan José <junogouvea@gmail.com>
2023-01-21 14:14:27 -0400

    frameworks update - add angular and create folders

--- front_end/TypeScript/concepts/2_functions.ts
+++ front_end/TypeScript/concepts/2_functions.ts
@@ -1,7 +1,5 @@
-/**
- * FUNCTIONS
- */
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
 // Parameter Type Annotations: it consists on declaring the type of a parameter on a function
 function triple(value: number){
   return value * 3;
@@ -13,6 +11,7 @@ function greetTripled(greeting: string, value: number) {
 
 greetTripled( 'Hiya',5);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Optional Parameters: if we don't put a value on the parameter when calling a function, this gives error.
 // Shouldn't be like that. To indicate that a parameter is intentionally optional, we add a ? after its name.
 function proclaimm(status?: string) {
@@ -33,3 +32,48 @@ function proclaim(status = 'not ready...', repeat = 1) {
 proclaim();
 proclaim('ready?');
 proclaim('ready!', 3);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Inferring Return Types:  It does this by looking at the types of the values after a function’s return statements.
+
+function createGreeting(name: string) {
+  return `Hello, ${name}!`;
+}
+ 
+const myGreeting = createGreeting('Aisle Nevertell');
+
+//Example
+function getRandomNumber() {
+  return Math.random();
+}
+
+let myVar = getRandomNumber();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Explicit Return Types:
+
+import {getUserChoice,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12} from './resources'
+function returnFruit():string{ //Here we specify the type of function. In this case, a string.
+  let num = getUserChoice();
+  switch(num){
+    case 1 : return f1();
+		case 2 : return f2();
+		case 3 : return f3();
+		case 4 : return f4();
+		case 5 : return f5();
+		case 6 : return f6();
+		case 7 : return f7();
+		case 8 : return f8();
+		case 9 : return f9();
+		case 10 : return f10();
+		case 11 : return f11();
+		case 12 : return f12();
+  }
+  return 'durian.jpg'
+}
+
+console.log(returnFruit())
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Void Return Type
+
